eff3bebd5ae71a2ae41d2c00cd51ed33b49d99f5
Rob <robburson07@gmail.com>
2022-01-29 15:21:16 -0600

    finished routes and api

---
+++ routes/api/users-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+const {
+    getAllUsers, getUserbyId, createUser, updateUser, deleteUser, addFriend, removeFriend
+} = require("../../controllers/user-controller");
+
+// /api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getUserbyId).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
