f39314c202a28206c3846c0a139cd151394e195e
Rob <robburson07@gmail.com>
2022-01-29 18:37:12 -0600

    wrote the server.js code

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// if MONGODB_URI exists, connect
+// else re-route to local MongoDB server
+// MongoDB finds/connects to DB if exists, creates new if not
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
+    useFindAndModify: false, 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+//log Mongo queries
+
+mongoose.set("debug", true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
